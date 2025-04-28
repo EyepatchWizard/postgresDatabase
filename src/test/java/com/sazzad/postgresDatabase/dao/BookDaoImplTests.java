0a77a5fe31319932d4d9d2f30502d8a8c2716be6
EyepatchWizard <shsazzadhossainy@gmail.com>
2025-04-29 03:19:18 +0600

    setting up unit test for DAO jdbc create book

--- src/test/java/com/sazzad/postgresDatabase/dao/BookDaoImplTests.java
+++ src/test/java/com/sazzad/postgresDatabase/dao/BookDaoImplTests.java
@@ -1,4 +1,44 @@
 package com.sazzad.postgresDatabase.dao;
 
+import com.sazzad.postgresDatabase.dao.impl.BookDaoImpl;
+import com.sazzad.postgresDatabase.domain.Author;
+import com.sazzad.postgresDatabase.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class BookDaoImplTests {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private BookDaoImpl underTest;
+
+    @Test
+    public void testThatCreateBookGeneratesCorrectSql(){
+
+       Book book = Book.builder()
+               .isbn("978-1-2345-6789-0")
+               .title("The Shadow In The Attic")
+               .authorId(1L)
+               .build();
+
+        underTest.create(book);
+
+        verify(jdbcTemplate).update(
+                eq("INSERT INTO books (isbn, title, authorId) VALUES (?, ?, ?,)"),
+                eq("978-1-2345-6789-0"),
+                eq("The Shadow In The Attic"),
+                eq(1L)
+        );
+    }
+
 }
