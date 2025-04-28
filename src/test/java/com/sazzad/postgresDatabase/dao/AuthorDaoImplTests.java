31866144bd91e5b8bdde0b826d8b21d6216cfc36
EyepatchWizard <shsazzadhossainy@gmail.com>
2025-04-29 02:58:22 +0600

    setting up unit test for DAO jdbc create

---
+++ src/test/java/com/sazzad/postgresDatabase/dao/AuthorDaoImplTests.java
@@ -0,0 +1,40 @@
+package com.sazzad.postgresDatabase.dao;
+
+import com.sazzad.postgresDatabase.dao.impl.AuthorDaoImpl;
+import com.sazzad.postgresDatabase.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorDaoImplTests {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private AuthorDaoImpl undertest;
+
+    @Test
+    public void testThatCreateAuthorGeneratesCorrectSql(){
+
+        Author author = Author.builder()
+                .id(1L)
+                .name("Abigail Rose")
+                .age(80)
+                .build();
+
+        undertest.create(author);
+
+        verify(jdbcTemplate).update(
+                eq("INSERT INTO authors (id, name, age) VALUES (?, ?, ?)"),
+                eq(1L),eq("Abigail Rose"),eq(80)
+        );
+    }
+}
